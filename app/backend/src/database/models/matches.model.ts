dc9c9b7bf316ce4a491603b7b4dc3b165e3ea5fe
Davi Alpiano <davialpiano@gmail.com>
2024-08-05 18:39:14 -0300

    tests 13

---
+++ app/backend/src/database/models/matches.model.ts
@@ -0,0 +1,59 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+import Team from './team.model';
+
+export default class Matches extends Model<InferAttributes<Matches>,
+InferCreationAttributes<Matches>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: number;
+}
+
+Matches.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'Matches',
+  tableName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+Team.hasMany(Matches, { foreignKey: 'homeTeamId', as: 'homeTeams' });
+Team.hasMany(Matches, { foreignKey: 'awayTeamId', as: 'awayTeams' });
+Matches.belongsTo(Team, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+Matches.belongsTo(Team, { foreignKey: 'awayTeamId', as: 'awayTeam' });
